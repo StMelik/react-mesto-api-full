d917b508586a041a5495a61f5de57ca7db8e1d5e
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-06-09 00:01:47 +0300

    feat: обновить бекенд

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,6 +1,8 @@
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const reqExpLink = /^(https?:\/\/)(www\.)?([\w-.~:/?#[\]@!$&')(*+,;=]*\.?)*\.{1}[\w]{2,8}(\/([\w-.~:/?#[\]@!$&')(*+,;=])*)?/;
 
-const secretKey = 'secret-key';
+const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'secret-key';
 
 module.exports = {
   reqExpLink,
